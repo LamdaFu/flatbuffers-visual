5214ad78afe83419fb8de5a61ac6684752e6d633
Ian Hunter <ian.hunter@intel.com>
2019-09-24 15:34:10 +0100

    updated the dependencies, but it turned out that it was just a hello world app :( well, at least I can give this back so people can access the hello world app faster

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['babel'],
+        loaders: ['babel-loader'],
         include: path.join(__dirname, 'src')
       },
       ,
